8a4bb7e1fea0921fa3bc9d90df8ec57198280d83
John Wilkes <jwilkes0011@gmail.com>
2025-01-11 13:14:50 -0600

    Creating joysticks

--- game.py
+++ game.py
@@ -1,5 +1,5 @@
 import pygame
-from pygame.locals import KEYDOWN, KEYUP, QUIT
+from pygame.locals import JOYDEVICEADDED, JOYDEVICEREMOVED, KEYDOWN, KEYUP, QUIT
 
 DEBUG_TEXT_COLOR = (180, 0, 150)
 
@@ -7,6 +7,8 @@ class Game:
     def __init__(self):
         pygame.init()
         pygame.font.init()
+        pygame.joystick.init()
+
         self._fps_clock = pygame.time.Clock()
         self._tick_delta = 0.0
 
@@ -17,14 +19,33 @@ class Game:
         self._can_change_debug = True
         self._debug_font = pygame.font.SysFont('Courier', 20)
 
+        self._joysticks: list[pygame.joystick.JoystickType] = []
+
     def _display_debug(self) -> None:
+        y = 0
+
         fps = self._fps_clock.get_fps()
-        fps_surface = self._debug_font.render(f'FPS: {fps:.1f}', False, DEBUG_TEXT_COLOR)
-        self._display_surf.blit(fps_surface, (0, 0))
+        text_surface = self._debug_font.render(f'FPS: {fps:.1f}', False, DEBUG_TEXT_COLOR)
+        self._display_surf.blit(text_surface, (0, y))
+        y += text_surface.get_rect().bottom
 
         tick_delta_ms = self._tick_delta * 1000
-        tick_delta_surface = self._debug_font.render(f'Tick delta: {tick_delta_ms:.1f} ms', False, DEBUG_TEXT_COLOR)
-        self._display_surf.blit(tick_delta_surface, (0, fps_surface.get_rect().bottom))
+        text_surface = self._debug_font.render(f'Tick delta: {tick_delta_ms:.1f} ms', False, DEBUG_TEXT_COLOR)
+        self._display_surf.blit(text_surface, (0, y))
+        y += text_surface.get_rect().bottom
+
+        joystick_count = pygame.joystick.get_count()
+        text_surface = self._debug_font.render(f'Joystick info ({joystick_count}):', False, DEBUG_TEXT_COLOR)
+        self._display_surf.blit(text_surface, (0, y))
+        y += text_surface.get_rect().bottom
+
+        for i in range(joystick_count):
+            # joystick_count = pygame.joystick.get_count()\
+            joystick = pygame.joystick.Joystick(i)
+            name = joystick.get_name()
+            text_surface = self._debug_font.render(f' {i}: {name}', False, DEBUG_TEXT_COLOR)
+            self._display_surf.blit(text_surface, (0, y))
+            y += text_surface.get_rect().bottom
 
     def mainloop(self) -> None:
         quit_game = False
@@ -40,6 +61,16 @@ class Game:
                 elif event.type == KEYUP:
                     if event.key == pygame.K_F1:
                         self._can_change_debug = True
+                elif event.type == JOYDEVICEADDED:
+                    joystick = pygame.joystick.Joystick(event.device_index)
+                    self._joysticks.append(joystick)
+                elif event.type == JOYDEVICEREMOVED:
+                    idx = event.instance_id
+                    for joystick in self._joysticks:
+                        if joystick.get_id() == idx:
+                            self._joysticks.pop(idx)
+                            break
+
             if quit_game:
                 break
 
