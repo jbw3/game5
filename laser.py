21193a5ee28cc910e3fd2952c42cbc5e04122697
John Wilkes <jwilkes0011@gmail.com>
2025-01-20 12:54:09 -0600

    Lasers now collide with asteroids

--- laser.py
+++ laser.py
@@ -33,6 +33,12 @@ class Laser(pygame.sprite.Sprite):
 
         self.rect.center = (int(self._x), int(self._y))
 
+        # remove laser when it goes beyond the bounds of the view
         view_width, view_height = game.flight_view_size
         if self._x < 0.0 or self._x >= view_width or self._y < 0.0 or self._y >= view_height:
+            game.flight_view_sprites.remove(self)
+
+        collide_sprites = pygame.sprite.spritecollide(self, game.flight_collision_sprites, False)
+        if len(collide_sprites) > 0:
+            collide_sprites[0].collide(game)
             game.flight_view_sprites.remove(self)
