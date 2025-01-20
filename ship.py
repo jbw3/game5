42f42af1a428a6a131686b71c6229798849a6b51
John Wilkes <jwilkes0011@gmail.com>
2025-01-20 10:31:20 -0600

    Adding weapon aiming

--- ship.py
+++ ship.py
@@ -1,3 +1,4 @@
+import math
 import os
 import pygame
 from typing import TYPE_CHECKING, override
@@ -27,13 +28,13 @@ class Console(pygame.sprite.Sprite):
     def person(self) -> Person|None:
         return self._person
 
-    def activate(self, person: Person) -> None:
+    def activate(self, ship: 'Ship', person: Person) -> None:
         self._person = person
 
         self.person.rect.centerx = self.rect.centerx
         self.person.rect.top = self.rect.bottom + 4
 
-    def deactivate(self) -> None:
+    def deactivate(self, ship: 'Ship') -> None:
         self._person = None
 
     def update_ship(self, game: 'Game', ship: 'Ship') -> None:
@@ -63,24 +64,82 @@ class PilotConsole(Console):
             ship.accelerate(x_accel, y_accel)
 
 class WeaponConsole(Console):
-    def __init__(self, game: 'Game'):
+    def __init__(self, game: 'Game', weapon_index: int):
         super().__init__(game, Console.WEAPON_CONSOLE_IMAGE)
+        self._weapon_index = weapon_index
+
+    @override
+    def activate(self, ship: 'Ship', person: Person) -> None:
+        super().activate(ship, person)
+        ship.enable_aiming(self._weapon_index)
+
+    @override
+    def deactivate(self, ship: 'Ship') -> None:
+        super().deactivate(ship)
+        ship.disable_aiming(self._weapon_index)
 
     @override
     def update_ship(self, game: 'Game', ship: 'Ship') -> None:
         if self._person is not None:
             joystick = self._person.joystick
 
+            a0 = joystick.get_axis(0)
+            if a0 < -0.2:
+                ship.rotate_aim_counterclockwise(self._weapon_index)
+            elif a0 > 0.2:
+                ship.rotate_aim_clockwise(self._weapon_index)
+
             if joystick.get_button(5):
-                ship.fire_laser(game)
+                ship.fire_laser()
+
+class AimSprite(pygame.sprite.Sprite):
+    LENGTH = 80
+
+    def __init__(self, color: tuple[int, int, int], origin: tuple[int, int]):
+        super().__init__()
+        self._orig_image = pygame.surface.Surface((AimSprite.LENGTH, 3))
+        self._orig_image.fill((0, 0, 0))
+        self._orig_image.set_colorkey((0, 0, 0))
+        pygame.draw.line(self._orig_image, color, (0, 1), (AimSprite.LENGTH - 1, 1))
+
+        self.image = self._orig_image
+        self.rect = self.image.get_rect()
+
+        self._origin = origin
+        self.angle = 90.0
+
+    @property
+    def angle(self) -> float:
+        return self._angle
+
+    @angle.setter
+    def angle(self, new_angle: float) -> None:
+        self._angle = new_angle % 360.0
+
+        self.image = pygame.transform.rotate(self._orig_image, new_angle)
+        self.rect = self.image.get_rect()
+        if 0.0 <= self._angle < 90.0:
+            self.rect.left = self._origin[0]
+            self.rect.bottom = self._origin[1]
+        elif 90.0 <= self._angle < 180.0:
+            self.rect.right = self._origin[0]
+            self.rect.bottom = self._origin[1]
+        elif 180.0 <= self._angle < 270.0:
+            self.rect.right = self._origin[0]
+            self.rect.top = self._origin[1]
+        else:
+            self.rect.left = self._origin[0]
+            self.rect.top = self._origin[1]
 
 class Ship:
     MAX_ACCELERATION = 5.0
     LASER_DELAY = 800 # ms
+    AIM_ANGLE_RATE = 90.0 # degrees
     FLOOR_COLOR = (180, 180, 180)
     WALL_COLOR = (80, 80, 80)
 
     def __init__(self, game: 'Game', interior_view_center: tuple[int, int]):
+        self.game = game
         flight_view_center = (game.flight_view_size[0] // 2, game.flight_view_size[1] // 2)
         self._x = float(flight_view_center[0])
         self._y = float(flight_view_center[1])
@@ -185,8 +244,8 @@ class Ship:
 
         # weapon consoles
         top = floor1.rect.top + 25
-        for _ in range(2):
-            weapon_console = WeaponConsole(game)
+        for i in range(2):
+            weapon_console = WeaponConsole(game, i)
             weapon_console.rect.left = floor1.rect.left
             weapon_console.rect.top = top
             self._consoles.append(weapon_console)
@@ -202,11 +261,22 @@ class Ship:
         self._flight_sprite.rect.center = flight_view_center
         game.flight_view_sprites.add(self._flight_sprite)
 
+        # weapon aiming
+        self._aiming: list[AimSprite] = []
+        colors = [
+            (255, 0, 0),
+            (0, 240, 0),
+        ]
+        for i in range(2):
+            origin = (self._flight_sprite.rect.centerx, self._flight_sprite.rect.centery)
+            aim_sprite = AimSprite(colors[i], origin)
+            self._aiming.append(aim_sprite)
+
     def try_activate_console(self, person: 'Person') -> bool:
         for console in self._consoles:
             console_rect: pygame.rect.Rect = console.rect.inflate(4, 20)
             if console.person is None and console_rect.colliderect(person.rect):
-                console.activate(person)
+                console.activate(self, person)
                 return True
 
         return False
@@ -214,17 +284,29 @@ class Ship:
     def deactivate_console(self, person: 'Person') -> None:
         for console in self._consoles:
             if console.person is person:
-                console.deactivate()
+                console.deactivate(self)
                 break
 
     def accelerate(self, x_accel: float, y_accel: float) -> None:
         self._dx += x_accel
         self._dy += y_accel
 
-    def fire_laser(self, game: 'Game') -> None:
+    def enable_aiming(self, weapon_index: int) -> None:
+        self.game.flight_view_sprites.add(self._aiming[weapon_index])
+
+    def disable_aiming(self, weapon_index: int) -> None:
+        self.game.flight_view_sprites.remove(self._aiming[weapon_index])
+
+    def rotate_aim_clockwise(self, weapon_index: int) -> None:
+        self._aiming[weapon_index].angle -= Ship.AIM_ANGLE_RATE * self.game.frame_time
+
+    def rotate_aim_counterclockwise(self, weapon_index: int) -> None:
+        self._aiming[weapon_index].angle += Ship.AIM_ANGLE_RATE * self.game.frame_time
+
+    def fire_laser(self) -> None:
         ticks = pygame.time.get_ticks()
         if ticks >= self._next_available_laser_fire:
-            Laser(game, self._flight_sprite.rect.center, 0.0)
+            Laser(self.game, self._flight_sprite.rect.center, 0.0)
             self._next_available_laser_fire = ticks + Ship.LASER_DELAY
 
     def update(self, game: 'Game') -> None:
