fd89520b1e1c62fe2db1cf97cbe710d9a713b7ba
John Wilkes <jwilkes0011@gmail.com>
2025-01-29 22:38:39 -0600

    Making door movement independent of frame rate

--- door.py
+++ door.py
@@ -15,11 +15,12 @@ class Door(Sprite):
 
     COLORKEY = (0, 0, 0)
     COLOR = (110, 120, 150)
+    MOVE_RATE = 60.0
 
     def __init__(self, game: 'Game', orientation: Orientation, gap_len: int, thickness: int):
         self._orientation = orientation
         self._gap_len = gap_len
-        self._current_len = self._gap_len // 2
+        self._current_len = self._gap_len / 2
         self._thickness = thickness
 
         if self._orientation == Door.Orientation.Horizontal:
@@ -36,41 +37,39 @@ class Door(Sprite):
         super().__init__(surface)
         game.interior_view_sprites.add(self)
 
-        self._last_update = pygame.time.get_ticks()
-
     @override
     def update(self, game: 'Game') -> None:
-        if pygame.time.get_ticks() >= self._last_update + 10:
+        if self._orientation == Door.Orientation.Horizontal:
+            proximity_rect = self.rect.inflate(8, 10)
+        else:
+            proximity_rect = self.rect.inflate(10, 8)
+
+        opening = False
+        for person in game.people_sprites:
+            if proximity_rect.colliderect(person.rect):
+                opening = True
+                break
+
+        needs_update = False
+        if opening:
+            if self._current_len > 1:
+                self._current_len -= game.frame_time * Door.MOVE_RATE
+                self._current_len = max(1.0, self._current_len)
+                needs_update = True
+        else:
+            if self._current_len < self._gap_len / 2:
+                self._current_len += game.frame_time * Door.MOVE_RATE
+                self._current_len = min(self._gap_len / 2, self._current_len)
+                needs_update = True
+
+        if needs_update:
+            current_len_int = int(self._current_len)
             if self._orientation == Door.Orientation.Horizontal:
-                proximity_rect = self.rect.inflate(8, 10)
+                rect1 = pygame.rect.Rect(0, 0, current_len_int, self._thickness)
+                rect2 = pygame.rect.Rect(self._gap_len - current_len_int, 0, current_len_int, self._thickness)
             else:
-                proximity_rect = self.rect.inflate(10, 8)
-
-            opening = False
-            for person in game.people_sprites:
-                if proximity_rect.colliderect(person.rect):
-                    opening = True
-                    break
-
-            needs_update = False
-            if opening:
-                if self._current_len > 1:
-                    self._current_len -= 1
-                    needs_update = True
-            else:
-                if self._current_len < self._gap_len // 2:
-                    self._current_len += 1
-                    needs_update = True
-
-            if needs_update:
-                if self._orientation == Door.Orientation.Horizontal:
-                    rect1 = pygame.rect.Rect(0, 0, self._current_len, self._thickness)
-                    rect2 = pygame.rect.Rect(self._gap_len - self._current_len, 0, self._current_len, self._thickness)
-                else:
-                    rect1 = pygame.rect.Rect(0, 0, self._thickness, self._current_len)
-                    rect2 = pygame.rect.Rect(0, self._gap_len - self._current_len, self._thickness, self._current_len)
-                self.image.fill(Door.COLORKEY)
-                pygame.draw.rect(self.image, Door.COLOR, rect1)
-                pygame.draw.rect(self.image, Door.COLOR, rect2)
-
-            self._last_update = pygame.time.get_ticks()
+                rect1 = pygame.rect.Rect(0, 0, self._thickness, current_len_int)
+                rect2 = pygame.rect.Rect(0, self._gap_len - current_len_int, self._thickness, current_len_int)
+            self.image.fill(Door.COLORKEY)
+            pygame.draw.rect(self.image, Door.COLOR, rect1)
+            pygame.draw.rect(self.image, Door.COLOR, rect2)
