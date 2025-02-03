0c49f5ef673d70ab5358f3b6f7363ba798944108
John Wilkes <jwilkes0011@gmail.com>
2025-02-02 19:34:22 -0600

    Sprite class now keeps its position when updating its image

--- sprite.py
+++ sprite.py
@@ -3,7 +3,8 @@ import pygame
 class Sprite(pygame.sprite.DirtySprite):
     def __init__(self, image: pygame.surface.Surface):
         super().__init__()
-        self.image = image
+        self._image = image
+        self.rect = self._image.get_rect()
 
     @property
     def image(self) -> pygame.surface.Surface:
@@ -11,5 +12,7 @@ class Sprite(pygame.sprite.DirtySprite):
 
     @image.setter
     def image(self, value: pygame.surface.Surface) -> None:
+        old_topleft = self.rect.topleft
         self._image = value
         self.rect = self._image.get_rect()
+        self.rect.topleft = old_topleft
