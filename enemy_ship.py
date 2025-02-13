98cf3d3700988fcac2df0b5d731de84757e88d01
John Wilkes <jwilkes0011@gmail.com>
2025-02-12 22:42:04 -0600

    Overriding collide() and damage() for EnemyShip

--- enemy_ship.py
+++ enemy_ship.py
@@ -14,7 +14,30 @@ class EnemyShip(FlightCollisionSprite):
         self.mask = pygame.mask.from_surface(self.image)
 
         game.flight_view_sprites.add(self)
+        game.flight_collision_sprites.add(self)
+
+        self._hull = 3
 
     @override
     def update(self, game: 'Game') -> None:
         pass
+
+    @override
+    def collide(self, game: 'Game', new_dx: float, new_dy: float, force: float) -> None:
+        self._dx = new_dx
+        self._dy = new_dy
+
+        hit_points = int(force / 10_000)
+        self._hull -= min(self._hull, hit_points)
+        if self._hull <= 0:
+            self.destroy()
+
+    @override
+    def damage(self, game: 'Game') -> None:
+        self._hull -= 1
+        if self._hull <= 0:
+            self.destroy()
+
+    def destroy(self) -> None:
+        # remove from all sprite groups
+        self.kill()
